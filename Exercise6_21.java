35a448aba44abf35204e69b1aa164c13c8e4a9bf
Michal Kornacki <mkornacki888@gmail.com>
2023-09-22 18:15:12 +0100

    done some work

--- Exercise6_21.java
+++ Exercise6_21.java
@@ -1,5 +1,33 @@
-public class Exercise6_21 {
+import java.util.Scanner;
+public class Exercise6_21 { 
+
+    public static String path(int slots){
+
+        int rows=slots;
+        String path = "";
+        for(int i=0;i<rows;i++){
+            int chance = (int) (Math.random()*2)+1;
+            if(chance==1){
+                path+="R";
+            }
+            else{
+                path+="L";
+            }
+        }
+        return path;
+
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the numbers of balls to drop: ");
+        int balls = scanner.nextInt();
+        System.out.println("Enter the number of slots in the bean machine: ");
+        int slots = scanner.nextInt();
         
+        for(int i=0;i<balls;i++){
+            System.out.println(path(slots));
+        }
+
+
     }
 }
